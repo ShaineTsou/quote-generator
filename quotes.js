4a2ebb1e68f44a128fde92ffaa77e8465438d344
Shaine Tsou <shaineagro06@gmail.com>
2021-08-05 16:34:48 +0800

    deleted the hyphen before author, created a js file for local quotes, and wrote functions to get quotes from api and randomly pick one quote

---
+++ quotes.js
@@ -0,0 +1,18 @@
+const localQuotes = [
+  {
+    text: 'Do not overestimate the competition and underestimate yourself. You are better than you think.',
+    author: 'Tim Ferriss'
+  },
+  {
+    text: 'Don\'t let failure go to your heart, and don\'t let success go to your head.',
+    author: 'Will Smith'
+  },
+  {
+    text: 'A ship in port is safe, but that\'s not what ships are built for.',
+    author: 'Grace Hopper'
+  },
+  {
+    text: 'If you never ask, the answer is always no.',
+    author: 'Nora Roberts'
+  }
+]
