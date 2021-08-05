82047ebabdfb1862be1c5fa823103e91e7478a6e
Shaine Tsou <shaineagro06@gmail.com>
2021-08-05 17:53:31 +0800

    made font-size of quote and author smaller, added media query for phone screen, made the quote dynamically changed upon loading and clicking the new quote button

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (quote.author) {
+    authorText.textContent = quote.author;
+  } else {
+    authorText.textContent = 'Unknown';
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 100) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
 }
 
 
@@ -22,3 +39,5 @@ async function getQuotes() {
 
 // On Load
 getQuotes();
+
+newQuoteBtn.addEventListener('click', newQuote);
